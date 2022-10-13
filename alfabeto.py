fa4cf14ec4df99ce92625db483e44de70290f3f6
Marcio Maia <88562590+casodio@users.noreply.github.com>
2022-10-13 12:22:10 -0300

    Create alfabeto.py
    criando o modulo e adicionando as letras A e B.

---
+++ alfabeto.py
@@ -0,0 +1,58 @@
+from turtle import Turtle
+
+t = Turtle()
+t.speed(1)
+t.pensize(3)
+
+
+# letra A
+def a():
+    t.color('black')
+    t.left(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(90)
+    t.right(90)
+    t.forward(50)
+    t.right(90)
+    t.forward(90)
+    t.back(90)
+    t.left(90)
+    t.forward(50)
+    t.left(90)
+    t.color('white')
+    t.forward(50)
+
+
+# letra B
+def b():
+    t.color('black')
+    t.left(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(80)
+    t.right(45)
+    t.forward(10)
+    t.right(45)
+    t.forward(30)
+    t.right(45)
+    t.forward(10)
+    t.right(45)
+    t.forward(80)
+    t.back(80)
+    t.right(180)
+    t.right(45)
+    t.forward(20)
+    t.right(45)
+    t.forward(30)
+    t.right(45)
+    t.forward(20)
+    t.right(45)
+    t.forward(80)
+    t.back(80)
+    t.color('white')
+    t.back(60)
+    t.color('white')
+    t.right(180)
+    t.forward(50)
+
